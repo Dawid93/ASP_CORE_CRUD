33bf511eb61f7f84a5bc9acc3ffb45e881c509ba
Dave <ddawid@windowslive.com>
2019-11-06 15:29:56 +0100

    Create sample beerItem

---
+++ Crud/Crud/Models/BeerItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud.Models
+{
+    public class BeerItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Brewery { get; set; } // Create Bewery model later
+        public byte[] LabelImage { get; set; }
+    }
+}
