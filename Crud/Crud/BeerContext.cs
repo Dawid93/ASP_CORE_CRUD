0a2d6dd900fba3d471c1bdf4823d83f12401bd66
Dave <ddawid@windowslive.com>
2019-11-06 17:09:14 +0100

    Create sample controller

---
+++ Crud/Crud/BeerContext.cs
@@ -0,0 +1,18 @@
+﻿using Crud.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud
+{
+    public class BeerContext : DbContext
+    {
+        public BeerContext(DbContextOptions<BeerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BeerItem> BeerItems { get; set; }
+    }
+}
