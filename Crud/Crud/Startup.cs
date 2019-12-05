533dfb4f532b10ff587b441a2211519408398583
Dave <ddawid@windowslive.com>
2019-12-05 18:55:57 +0100

    Add autoMapper

--- Crud/Crud/Startup.cs
+++ Crud/Crud/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Crud.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -27,7 +28,8 @@ namespace Crud
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddXmlDataContractSerializerFormatters();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddScoped<IBeerRepository, BeerSqlRepository>();
             services.AddDbContext<BeerDbContext>(options =>
             {
