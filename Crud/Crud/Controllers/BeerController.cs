4250ac16ec691591d0be95d2b91845e0b415651a
Dave <ddawid@windowslive.com>
2019-12-05 18:56:12 +0100

    Add Empty beerController

---
+++ Crud/Crud/Controllers/BeerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Crud.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crud.Controllers
+{
+    [ApiController, Route("api/beer")]
+    public class BeerController : ControllerBase
+    {
+        private readonly IBeerRepository beerRepository;
+        private readonly IMapper mapper;
+
+        public BeerController(IBeerRepository beerRepository, IMapper mapper)
+        {
+            this.beerRepository = beerRepository;
+            this.mapper = mapper;
+        }
+    }
+}
