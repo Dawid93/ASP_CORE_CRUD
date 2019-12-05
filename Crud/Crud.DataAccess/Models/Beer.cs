dbfb4044164a95d8e65ee7de3a495d9caa613b2c
Dave <ddawid@windowslive.com>
2019-12-05 17:11:09 +0100

    Create simple beer model

---
+++ Crud/Crud.DataAccess/Models/Beer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Crud.DataAccess.Models
+{
+    public class Beer
+    {
+        [Key]
+        public Guid BeerId { get; set; }
+        [Required]
+        public string BeerName { get; set; }
+        [MaxLength(1500)]
+        public string BeerDescription { get; set; }
+    }
+}
