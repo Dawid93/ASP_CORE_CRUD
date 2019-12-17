a3ba5171af4b4362b6023e13d371b764909dc101
Dave <ddawid@windowslive.com>
2019-12-17 20:53:13 +0100

    Fix missing component

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { BeerCollectionComponent } from './beer/beer-collection/beer-collection.component';
 import { BeerFormComponent } from './beer/beer-form/beer-form.component';
 import { BeerDetailComponent } from './beer/beer-detail/beer-detail.component';
-import { BeerTypeComponent } from './beer/beer-form/beer-type/beer-type.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +14,6 @@ import { BeerTypeComponent } from './beer/beer-form/beer-type/beer-type.componen
     BeerCollectionComponent,
     BeerFormComponent,
     BeerDetailComponent,
-    BeerTypeComponent
   ],
   imports: [
     BrowserModule,
